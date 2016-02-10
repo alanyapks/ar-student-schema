54a2c7857e889aeed9bfd8bf2f292b7ced371b73
Next <nextacademy@email.com>
2016-02-10 04:31:24 -0800

    Created ORM for Student Class

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,12 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+	def name
+    "#{first_name} #{last_name}"
+  end
+
+	def age
+	    now = Time.now.utc.to_date
+	    now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+	  end
 end
